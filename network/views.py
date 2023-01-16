3a1965a364fd31d78df742169df909e144055ecc
ramonfrombr <ramonfrombr@gmail.com>
2023-01-16 14:21:47 -0300

    final build

--- network/views.py
+++ network/views.py
@@ -10,8 +10,10 @@ from django.http import JsonResponse
 import json
 from django.core.paginator import Paginator
 
+
 class NewPostForm(forms.Form):
     content = forms.CharField(max_length=500, label="", widget=forms.Textarea(attrs={'rows': 2, 'placeholder': "Write something..."}))
+
 
 def index(request):
 
@@ -22,6 +24,7 @@ def index(request):
             newPost.save()
 
     return HttpResponseRedirect(reverse('page', args=(1,)))
+
 
 def page(request, page_number):
 
@@ -43,50 +46,33 @@ def page(request, page_number):
 
     return render(request, "network/index.html",{"form": form,"posts": paginator.page(page_number), "num_pages": range(paginator.num_pages)})
 
-@csrf_exempt
-def like(request, post_id):
 
-    post = Post.objects.get(id=post_id)
+def following(request):
+    return HttpResponseRedirect(reverse('following_post_page', args=(1,)))
 
-    like = Like.objects.filter(post=post,user=request.user)
 
-    if like:
-        print(like[0])
-        like[0].delete()
+def following_post_page(request, page_number):
 
-        return JsonResponse({
-            "action": "unlike"
-        }, status=200)
-    else:
-        like = Like(post=post, user=request.user)
-        like.save()
-
-        return JsonResponse({
-            "action": "like"
-        }, status=200)
-
-@csrf_exempt
-def edit_post(request, post_id):
-
-    json_string = request.body.decode('utf8').replace("'", '"')
-    data = json.loads(json_string)
-    post = Post.objects.get(id=post_id)
-    post.content = data["content"]
-    post.save()
-
-    print(data["content"])
-
-    return JsonResponse({
-            "action": "edit"
-        }, status=200)
-
-def posts_following(request):
     followers = [follow.following_user for follow in list(request.user.following.all())]
+
     posts = Post.objects.filter(user__in=followers).order_by('-created_at')
-    return render(request, "network/posts_following.html", {"posts": posts})
+
+    postlist = []
+
+    for post in posts:
+        postObj = {"post": post, "user_liked": False}
+        for like in list(post.likes.all()):
+            if request.user == like.user:
+                postObj["user_liked"] = True
+                break   
+        postlist.append(postObj)
+
+    paginator = Paginator(postlist, 10)
+
+    return render(request, "network/following.html",{"posts": paginator.page(page_number), "num_pages": range(paginator.num_pages)})
+
 
 def profile(request, username):
-    #return HttpResponseRedirect(reverse('index'))
     return HttpResponseRedirect(reverse('profile_post_page', args=(username,1,)))
 
 
@@ -110,12 +96,58 @@ def profile_post_page(request, username, page_number):
 
         paginator = Paginator(postlist, 10)
 
-        print(">>> Page: " + str(paginator.page(1)[0]["post"].user.username))
-
         return render(request, "network/profile.html", {"profile_user": user, "followers": followers, "posts": paginator.page(page_number), "num_pages": range(paginator.num_pages)})
     else:
         print("ERROR: NO USER")# ERROR
         return HttpResponseRedirect(reverse('index'))
+
+
+@csrf_exempt
+def like(request, post_id):
+
+    post = Post.objects.get(id=post_id)
+
+    like = Like.objects.filter(post=post,user=request.user)
+
+    if like:
+        print(like[0])
+        like[0].delete()
+
+        return JsonResponse({
+            "action": "unlike"
+        }, status=200)
+    else:
+        like = Like(post=post, user=request.user)
+        like.save()
+
+        return JsonResponse({
+            "action": "like"
+        }, status=200)
+
+
+@csrf_exempt
+def edit_post(request, post_id):
+    post = Post.objects.get(id=post_id)
+
+    if post.user == request.user:
+
+        json_string = request.body.decode('utf8').replace("'", '"')
+        data = json.loads(json_string)
+        post = Post.objects.get(id=post_id)
+        post.content = data["content"]
+        post.save()
+
+        return JsonResponse({
+                "status": "success",
+                "message": "Post editted successfully."
+            }, status=200)
+    else:
+        return JsonResponse(
+            data={
+                "status": "error",
+                "message": "User is not the post author."
+            }, status=403
+        )
 
 
 def follow(request, username):
@@ -126,11 +158,13 @@ def follow(request, username):
         follow.save()
     return HttpResponseRedirect(reverse('profile', args=(username,)))
 
+
 def unfollow(request, username):
     following_user = User.objects.get(username=username)
     follow = UserFollowing.objects.filter(user=request.user, following_user=following_user)[0]
     follow.delete()
     return HttpResponseRedirect(reverse('profile', args=(username,)))
+
 
 def login_view(request):
     if request.method == "POST":
@@ -151,9 +185,11 @@ def login_view(request):
     else:
         return render(request, "network/login.html")
 
+
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
+
 
 def register(request):
     if request.method == "POST":
@@ -180,3 +216,4 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
